2487b4b98cff82b8d2501e635d14e3db017c2e28
Dharun-069 <manidharun2204@gmail.com>
2023-04-27 14:24:45 +0530

    feat: add some api in home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,25 +1,54 @@
 
 
 // import {axios} from './axios.js';
-let axios = require('axios');
-function ifsc_api(ifsc_branch) {
+// let axios = require('axios');
+// function ifsc_api(ifsc_branch) {
 
-    const options = {
-        method: 'GET',
-        url: `https://api.sandbox.co.in/bank/${ifsc_branch}`, //xml--><?xml><?brach>Kallakurichi</branch></xml> & JSON[{}]  
-        // `../${ifsc_branch}`
-        headers: { accept: 'application/json', 'x-api-version': '1.0' }
-    };
-    axios
-        .request(options)
-        .then(function (response) {
-            console.log(response.data.BRANCH);
-            // return response.data.BRANCH;
-            // console.log(response.data.BRANC4H)
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+//     const options = {
+//         method: 'GET',
+//         url: `https://api.sandbox.co.in/bank/${ifsc_branch}`, //xml--><?xml><?brach>Kallakurichi</branch></xml> & JSON[{}]  
+//         // `../${ifsc_branch}`
+//         headers: { accept: 'application/json', 'x-api-version': '1.0' }
+//     };
+//     axios
+//         .request(options)
+//         .then(function (response) {
+//             console.log(response.data.BRANCH);
+//             // return response.data.BRANCH;
+//             // console.log(response.data.BRANC4H)
+//         })
+//         .catch(function (error) {
+//             console.error(error);
+//         });
 
+// }
+// ifsc_api("CNRB0000973");
+
+
+// function ifsc_api(ifsc_branch) {
+//     fetch(`https://api.sandbox.co.in/bank/${ifsc_branch}`)
+//     // fetch('https://api.sandbox.co.in/bank/CNRB0000973')
+//       .then((results) => {
+//         return results.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//         // return data;
+//         // Access your data here
+//       });
+//   }
+//   ifsc_api("ABHIRAMAPURAM");
+//   // "ABHIRAMAPURAM, CHENNAI"
+
+function getUsers() {
+  fetch("https://api.sandbox.co.in/bank")
+    .then((results) => {
+      return results.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // return data;
+      // Access your data here
+    });
 }
-ifsc_api("CNRB0000973");
+getUsers();
